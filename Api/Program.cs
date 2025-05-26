a9655337769b8efef51c064c66bcb3bba40d8ba2
jaredm776 <jaredm776@cox.net>
2025-05-26 16:25:36 -0500

    fix auth

--- Api/Program.cs
+++ Api/Program.cs
@@ -69,8 +69,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseCors("BlazorCors");
-app.UseAuthorization();
 app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
